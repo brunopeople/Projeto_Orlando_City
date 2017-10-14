110791ff0b8d6c4897a67320bd4c1ff23ca5a6e1
brunopeople <brunopessoa.melo@gmail.com>
2017-10-14 00:45:20 -0300

    site loja virtual criada

---
+++ inc/php-calculo-frete-master/frete.php
@@ -0,0 +1,81 @@
+<?php
+/**
+*@name php-calcular-frete-correios
+* @version 0.1
+* @author Joâo Rangel
+* @license MIT license - http://www.opensource.org/licenses/mit-license.php
+*
+*/
+
+class Frete{
+	const URL = "http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx;CalcPrecoPrazo?";
+	const SERVICE = "40010";
+	private $xml;
+
+
+	public function __contrusct(
+		$CEPorigem,
+		$CEPdestino,
+		$peso,
+		$comprimento,
+		$altura,
+		$largura,
+		$valor
+
+	){
+
+		if($comprimento < 16) $comprimento = 16;
+
+		$this->xml = simplexml_load_file(Frete::URL."nCdEmpresa=&sDsSenha=&sCepOrigem=".$CEPorigem."&sCepDestino=".$CEPdestino."&nVlPeso=".$peso."&nCdFormato=1&nVlComprimento=".$comprimento."&nVlAltura=".$altura."&nVlLargura=".$largura."&sCdMaoPropria=n&nVlValorDeclarado=".$valor."&sCdAvisoRecebimento=n&nCdServico=".Frete::SERVICE."&nVlDiametro=0&StrRetorno=xml");
+
+		if(!$this->xml->Serviços->cServico){
+			throw new Exception("Error Processing Request", 400);
+		}
+
+		if($this->xml->Serviços->cServico->Erro!='0' && !$this->xml->Servicos->cServico->Erro == '010'){
+			throw new Exception($this->xml->Servicos->cServico->MsgErro, 400);
+		}
+	}
+
+
+	public function getValor(){
+		return(float)str_replace(',','.',$this->xml->Servicos->cServico->Valor);
+	}
+
+	public function getPrazoEntrega(){
+		return (int)$this->xml->Servico->cServico->PrazoEntrega;
+	}
+
+	public function getValorSemAdicionais(){
+		return(float)str_replace(',','.',$this->xml->Servicos->cServico->ValorSemAdicionais);
+	}
+
+	public function getValorMaoPropria(){
+		return(float)str_replace(',','.',$this->xml->Servico->cServico->ValorMaoPropria);
+	}
+
+	public function getValorAvisoRecebimento(){
+		return(float)str_replace(',', '.',$this->xml->Servicos->cServico->ValorAvisoRecebimento);
+	}
+
+	public function getValorValorDeclarado(){
+
+		return (float)str_replace(',', '.', $this->xml->Servicos->cServico->ValorValorDeclarado);
+
+	}
+
+	public function getMsgErro(){
+
+		return $this->xml->Servicos->cServico->MsgErro;
+
+	}
+
+	public function getObs(){
+
+		return $this->xml->Servicos->cServico->obsFim;
+
+	}
+}
+
+
+?>
---
+++ view/loja.php
@@ -0,0 +1,35 @@
+<?php include_once("header.php");?>
+
+		<section>
+			
+	<div class="container">
+		<div id="destaque-produtos">
+			<div class="item">
+				<div class=col-sm-6>
+					  <img src="img/produtos/moto-x.png" alt="Nome do Produto">
+				</div>
+
+		<div class=col-sm-6>
+			<h2>Smartphone Motorola Moto X Play Dual Chip Desbloqueado Android 5.1</h2>]
+				<div class="box-valor">
+					<div class="text-noboleto">no boleto</div>
+					<div class="text-por">Por</div>
+					<div class="text-reais">R$</div>
+					<div class="text-valor">1.259</div>
+					<div class="text-valor-centavos">,10</div>
+					<div class="text-parcelas">ou em até 8x de R$ 174,88</div>
+					<div class="text-total">total a prazo R$ 1.399,00</div>
+					<a href="#" class="btn btn-comprar"><i class="fa fa-shopping-cart"></i>Compre Agora</a>
+
+				   </div>
+				</div>
+			</div>
+		</div>
+	<button type="button" id="btn-destaque-prev"><i class="fa fa-angle-left"></i></button>
+	<button type="button" id="btn-destaque-next"><i class="fa fa-angle-right"></i></button>
+	
+	</div>
+
+		</section>
+
+<?php include_once("footer.php");?>
